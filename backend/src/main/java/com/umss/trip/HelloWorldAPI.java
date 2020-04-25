dc7a65a282336832b3c18899f4b4c8bb587d18d0
mlopez383 <mlopez383@gmail.com>
2020-04-25 12:55:08 -0400

    API call from React App implementation

--- backend/src/main/java/com/umss/trip/HelloWorldAPI.java
+++ backend/src/main/java/com/umss/trip/HelloWorldAPI.java
@@ -1,12 +1,19 @@
 package com.umss.trip;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:3000")
 public class HelloWorldAPI {
 	@GetMapping("/hello")
-	public String hello() {
-		return "Hello World from the Server";
+	public List<String> hello() {
+		ArrayList<String> list = new ArrayList<String>();
+		list.add("Hello World from the Server UMSS people");
+		return list;
 	}
 }
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,25 +2,33 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {mensaje: ""};
+	}
+
+	componentDidMount() {
+		fetch('http://localhost:3001/hello')
+		.then(res => res.json())
+		.then((data) => {
+			this.setState({
+				mensaje: data
+			});
+		})
+		.catch(console.log);
+	}
+
+	render(){
+		return (
+				<div className="App">
+				<header className="App-header">
+				<p>{this.state.mensaje}</p>
+				<img src={logo} className="App-logo" alt="logo" />
+					</header>
+				</div>
+		);
+	}
 }
 
 export default App;
