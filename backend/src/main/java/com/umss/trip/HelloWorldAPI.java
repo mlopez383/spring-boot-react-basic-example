3df54d8b71d0431360eea3cbe14348f952f8ad12
Miguel Angel Lopez Mamani <mlopez383@gmail.com>
2020-04-29 17:42:37 -0400

    Update cross origin allow all ports

--- backend/src/main/java/com/umss/trip/HelloWorldAPI.java
+++ backend/src/main/java/com/umss/trip/HelloWorldAPI.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class HelloWorldAPI {
 	@GetMapping("/hello")
 	public List<String> hello() {
